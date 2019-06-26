4e946e83e4d883ca98cf7f126fc993103a1e9f8c
David Kerekes <davkedav@gmail.com>
2019-06-26 08:12:04 +0200

    pages folder and its contents added

---
+++ folders.js
@@ -0,0 +1,33 @@
+import { withRouter } from 'next/router';
+import Udacity from '../elements/Udacity';
+import Codecademy from '../elements/Codecademy';
+import FreeCodeCamp from '../elements/FreeCodeCamp';
+import Personal from '../elements/Personal';
+
+const Folders = withRouter(props => {
+
+    const title = props.router.query.title;
+    let folder = null;
+
+    switch (title) {
+      case 'Udacity':
+        folder = <Udacity title={title}/>; break; 
+      case 'Codecademy':
+        folder = <Codecademy title={title}/>; break; 
+      case 'freeCodeCamp':
+        folder = <FreeCodeCamp title={title}/>; break; 
+      case 'Personal':
+        folder = <Personal title={title}/>; break; 
+      default:
+        folder = null; break;
+    } 
+
+    return (
+      <div>
+        {folder}
+      </div>
+    )
+  }
+);
+
+export default Folders;
---
+++ index.js
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import Frame from '../frame/Frame';
+import Footer from '../frame/Footer';
+import projectList from '../elements/projectList';
+import Articles from '../elements/Articles';
+
+
+const ProjectLink = props => (
+  <article className={props.cls}>
+    <Link href={`/folders?title=${props.title}`}>
+      <a href={props.title}>
+        <h2>{props.title}</h2>
+        <img alt={props.alt} src={props.imgSrc} />
+      </a>
+    </Link>
+    <p> {props.description} </p>
+  </article>
+);
+
+
+const Index = props => {
+  const folders = projectList.map((project, index) => (
+    <ProjectLink key={index + '_' + project.title} cls={project.cls} title={project.title}
+                 imgSrc={project.imgSrc} description={project.description} alt={project.alt} />
+  ));
+
+
+  return (
+    <div>
+      <Frame>
+        <div className="projectGrid">
+          {folders}
+        </div>
+      </Frame>
+      <Articles /> 
+      <Footer />
+    </div>
+  );
+}
+
+
+export default Index;
